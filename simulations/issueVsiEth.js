5e2a14df66f427aa985a53719cc92c173ff1d259
Mike Calvanese <mfcalvanese@gmail.com>
2017-09-24 13:48:07 -0600

    add buy VSI-ETH and get balance to simulation

--- simulations/issueVsiEth.js
+++ simulations/issueVsiEth.js
@@ -1,15 +1,20 @@
 import { web3Provider, web3 } from '../utils/w3'
 import Versi from 'versi'
 
+const { accounts } = web3.eth
+
 export default async function () {
   await doThing()
 }
 
 async function doThing() {
   const versi = new Versi(web3Provider, web3, {
-    from: web3.eth.accounts[0],
+    from: accounts[0],
     gas: 4500000
   })
-  const tx = await versi.issueVersiEtherToken()
+  const tx = await versi.buyVersiEther(2.5)
   console.log(tx.output())
+
+  const b = await versi.versiEtherBalance(accounts[0])
+  console.log(`balance=${b}`)
 }
