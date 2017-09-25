824190a461e28d7eac0c7823c8efe5d45d918769
Mike Calvanese <mfcalvanese@gmail.com>
2017-09-24 21:30:23 -0600

    buy and sell, log values in between

--- simulations/issueVsiEth.js
+++ simulations/issueVsiEth.js
@@ -3,20 +3,28 @@ import Versi from 'versi'
 
 const { accounts } = web3.eth
 
+let versi
+
 export default async function () {
   let tx
-  const versi = new Versi(web3Provider, web3, {
+  versi = new Versi(web3Provider, web3, {
     from: accounts[0],
     gas: 4500000
   })
   tx = await versi.buyVersiEther(3)
   console.log(tx.output())
 
-  // tx = await versi.sellVersiEther(1)
-  // console.log(tx.output())
+  await logBalances()
 
+  tx = await versi.sellVersiEther(3)
+  console.log(tx.output())
+  
+  await logBalances()
+
+}
+
+async function logBalances() {
   const b = await versi.versiEtherBalance(accounts[0])
   console.log(`VSI-ETH balance for account[0]=${b}`)
-
   console.log(`pool balance = ${await versi.poolValue()}`)
 }
