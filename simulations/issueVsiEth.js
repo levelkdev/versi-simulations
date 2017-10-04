4e45a01c051b340d0022bf051e771ad166d52466
Mike Calvanese <mfcalvanese@gmail.com>
2017-10-04 15:16:02 -0400

    update for refactored versi.js API

--- simulations/issueVsiEth.js
+++ simulations/issueVsiEth.js
@@ -1,30 +1,28 @@
 import { web3Provider, web3 } from '../utils/w3'
-import Versi from 'versi'
+import { VersiProvider } from 'versi'
 
 const { accounts } = web3.eth
 
-let versi
-
 export default async function () {
   let tx
-  versi = new Versi(web3Provider, web3, {
+  const versi = VersiProvider(web3Provider, web3, {
     from: accounts[0],
     gas: 4500000
   })
-  tx = await versi.buyVersiEther(3)
+  tx = await versi.exchange.buyVersiEther(3)
   console.log(tx.output())
 
   await logBalances()
 
-  tx = await versi.sellVersiEther(3)
+  tx = await versi.exchange.sellVersiEther(3)
   console.log(tx.output())
   
   await logBalances()
 
-}
+  async function logBalances() {
+    const b = await versi.exchange.versiEtherBalance(accounts[0])
+    console.log(`VSI-ETH balance for account[0]=${b}`)
+    console.log(`pool balance = ${await versi.pool.value()}`)
+  }
 
-async function logBalances() {
-  const b = await versi.versiEtherBalance(accounts[0])
-  console.log(`VSI-ETH balance for account[0]=${b}`)
-  console.log(`pool balance = ${await versi.poolValue()}`)
 }
